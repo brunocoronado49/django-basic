2d410c0057e476d94da676641ac5aa237ca5a43c
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2023-03-07 22:39:42 -0600

    Agreganbdo estilos

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Project, Task
 from .forms import CreateNewTask, CreateNewProject
